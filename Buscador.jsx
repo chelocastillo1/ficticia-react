028438f78f82e4cbed921b8897b9d6ad892ceb94
Chelo <16605426+chelocastillo1@users.noreply.github.com>
2022-07-17 02:39:49 -0300

    [fix] mensaje de error

--- Buscador.jsx
+++ Buscador.jsx
@@ -145,7 +145,7 @@ function Buscador(props) {
                     <Row>
                         <Col className="text-break">
                             <strong>Mensaje:</strong>&nbsp;{ array_errors.filter((e) => e.code === (error.response.data != null ? error.response.data.code : error.code))
-                                                            .map(f => {f.text, console.log(f)}) }
+                                                            .map(f => (error.response.data != null ? error.response.data : error).text) }
                         </Col>
                     </Row>
                 </Alert>
